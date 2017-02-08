f7daab69b1c4adbf948c7bf950dd899bbcceabca
mathewsmurie1 <mathews.obwana@savannahinformatics.com>
2017-02-08 20:43:25 +0300

    Fix the orders module and finalize the simple online store

--- django_store/orders/views.py
+++ django_store/orders/views.py
@@ -17,8 +17,8 @@ def order_create(request):
             cart.clear()
             return render(request,
                 'orders/order/created.html',
-                {'order': order}
-            else:
-                form = OrderCreateForm()
-                return render(request,'orders/order/create.html',
+                {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request,'orders/order/create.html',
                     {'cart': cart, 'form': form})
